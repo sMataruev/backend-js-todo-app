968f37fb78ed2c70fbe7401de9d8092f8237d41d
M Sergey <crow.866@gmail.com>
2019-11-27 18:38:19 +0300

    add js start file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
 'use strict';
+window.addEventListener( 'DOMContentLoaded', () => {
+    const task = [
+        {
+            id: '1',
+            title: 'New Task',
+            body: 'Some Text for task'
+        },
+        {
+            id: '2',
+            title: 'New Task 2',
+            body: 'Some Text for task'
+        },
+        {
+            id: '3',
+            title: 'New Task 3',
+            body: 'Some Text for task'
+        }
+    ];
+
+    const form = document.forms[ 'taskForm' ];
+    const inputTitle = form[ 0 ];
+    const inputBody = form[ 1 ];
 
 
+    form.addEventListener( 'submit', onFormSubmit );
+
+    function onFormSubmit( e ) {
+        e.preventDefault();
+        
+
+    }
+
+
+} );
